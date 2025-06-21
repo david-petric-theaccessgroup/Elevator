d01975cca9eb40d8cf2bf9c7deeb416d9b0fc05d
David Petric <david.petric@theaccessgroup.com>
2025-06-21 17:49:55 +0300

    Add project files.

---
+++ Elevator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Elevator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
